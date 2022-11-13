6674bbe19efb7931990a74851af3afbfa9af5140
yoshiioko <acardoza@pdx.edu>
2022-11-12 23:39:51 -0800

    Saving token name and symbol

--- contracts/XYZ.sol
+++ contracts/XYZ.sol
@@ -7,6 +7,8 @@ contract XYZ {
     string private _symbol;
 
     constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
     }
 
     function name() public view returns (string memory) {
