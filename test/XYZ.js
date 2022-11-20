ff0ba2189230af4d9f84676877dd4831bb54bd6a
yoshiioko <acardoza@pdx.edu>
2022-11-20 11:00:27 -0800

    Organize sanity tests together

--- test/XYZ.js
+++ test/XYZ.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 const hre = require("hardhat");
 const { ethers } = require("hardhat");
 
-describe("XYZ", function () {
+describe("XYZ Tests", function () {
   let xyz, signer;
 
   before("Deploy an instance of the contract first", async function () {
@@ -13,11 +13,21 @@ describe("XYZ", function () {
     [signer] = await ethers.provider.listAccounts();
   });
 
-  it("Validate token name", async function () {
-    expect(await xyz.name()).to.equal("XYZ Token");
-  });
+  describe("Sanity Tests", function () {
+    it("Validate token name", async function () {
+      expect(await xyz.name()).to.equal("XYZ Token");
+    });
 
-  it("Validate token symbol", async function () {
-    expect(await xyz.symbol()).to.equal("XYZ");
+    it("Validate token symbol", async function () {
+      expect(await xyz.symbol()).to.equal("XYZ");
+    });
+
+    it("Validate decimals value", async function () {
+      expect(await xyz.decimals()).to.equal(18);
+    });
+
+    it("Validate total supply", async function () {
+      expect(await xyz.totalSupply()).to.equal(1000000);
+    });
   });
 });
