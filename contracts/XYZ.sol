dc74ae73bbc977b2d259a9d148264ca6e33674c7
yoshiioko <acardoza@pdx.edu>
2022-11-20 11:01:03 -0800

    Add decimal and tokenSupply state variables and getter functions.

--- contracts/XYZ.sol
+++ contracts/XYZ.sol
@@ -5,10 +5,15 @@ pragma solidity ^0.8.17;
 contract XYZ {
     string private _name;
     string private _symbol;
+    uint8 private _decimals;
+
+    uint private _tokenSupply;
 
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
         _symbol = symbol_;
+        _decimals = 18;
+        _tokenSupply = 1_000_000;
     }
 
     function name() public view returns (string memory) {
@@ -17,5 +22,13 @@ contract XYZ {
 
     function symbol() public view returns (string memory) {
         return _symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() public view returns (uint) {
+        return _tokenSupply;
     }
 }
